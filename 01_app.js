efb0e78db2c1493ccbe0d7564bb2bed1d5b761ac
Abhishek Yadav <36642139+AbhishekYadavZ@users.noreply.github.com>
2022-06-10 10:58:48 +0000

    main

---
+++ 01_app.js
@@ -0,0 +1,9 @@
+const amount = 1
+if (amount<10){
+    console.log('small number');
+}
+else{
+    console.log("big number");
+}
+
+console.log("Hey its my first node app");
---
+++ 02_app.js
@@ -0,0 +1,4 @@
+console.log(__dirname);
+setInterval(() => {
+    console.log("Hey its my first node app");
+},1000);
